3714becd5a06684c81ecba7f90efc1700387d654
JHoldsworth23 <jacobph101@icloud.com>
2023-12-04 20:38:53 +0000

    Add the condition if the input is valid or not

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const inputElements = document.querySelectorAll("input");
 
 inputElements.forEach(input => {
-    console.log(input)
-})
+    input.addEventListener('change', (e) => {
+        const isValid = e.target.checkValidity();
+        if (!isValid) {
+            e.target.classList.remove("valid");
+            e.target.classList.add("invalid");
+        } else {
+            e.target.classList.remove("invalid");
+            e.target.classList.add("valid");
+        }
+    });
+});
