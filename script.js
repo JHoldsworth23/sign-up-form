6c1895ae955416b985ecc828885cb3c467fd34b1
JHoldsworth23 <jacobph101@icloud.com>
2023-12-04 21:33:10 +0000

    Add addEventListener for any invalid input

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const formSection = document.querySelector(".form-section");
 const submit = document.querySelector("#submit-btn");
 
 inputElements.forEach(input => {
+    input.addEventListener("invalid", (e) => {
+        e.target.classList.remove("valid");
+        e.target.classList.add("invalid");
+    })
+
     input.addEventListener('change', (e) => {
         const isValid = e.target.checkValidity();
-        if (!isValid) {
-            e.target.classList.remove("valid");
-            e.target.classList.add("invalid");
-        } else {
+        if (isValid) {
             e.target.classList.remove("invalid");
             e.target.classList.add("valid");
         }
