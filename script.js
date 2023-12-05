6a3832b1e6b81c936c8cfb8f2f6c962b7c37e178
JHoldsworth23 <jacobph101@icloud.com>
2023-12-05 18:26:05 +0000

    Add else statement when the password is match and change double quote mark into a single quote mark

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-const inputElements = document.querySelectorAll("input");
-const formSection = document.querySelector(".form-section");
-const password = document.querySelector("#pwd");
-const passwordConfirmation = document.querySelector("#pwd-confirm");
-const passwordAlert = document.querySelector("#pwd-alert");
-const submit = document.querySelector("#submit-btn");
+const inputElements = document.querySelectorAll('input');
+const formSection = document.querySelector('.form-section');
+const password = document.querySelector('#pwd');
+const passwordConfirmation = document.querySelector('#pwd-confirm');
+const passwordAlert = document.querySelector('#pwd-alert');
+const submit = document.querySelector('#submit-btn');
 
 inputElements.forEach(input => {
-    input.addEventListener("invalid", (e) => {
-        e.target.classList.remove("valid");
-        e.target.classList.add("invalid");
+    input.addEventListener('invalid', (e) => {
+        e.target.classList.remove('valid');
+        e.target.classList.add('invalid');
     })
 
     input.addEventListener('change', (e) => {
         const isValid = e.target.checkValidity();
         if (isValid) {
-            e.target.classList.remove("invalid");
-            e.target.classList.add("valid");
+            e.target.classList.remove('invalid');
+            e.target.classList.add('valid');
         }
     });
 });
 
-passwordConfirmation.addEventListener("change", (e) => {
+passwordConfirmation.addEventListener('change', (e) => {
     if (password.value !== e.target.value) {
-        passwordAlert.textContent = "* Passwords do not match";
-        password.classList.replace("valid", "invalid");
+        passwordAlert.textContent = '* Passwords do not match';
+        password.classList.replace('valid', 'invalid');
         password.value = '';
-        e.target.classList.replace("valid", "invalid");
+        e.target.classList.replace('valid', 'invalid');
         e.target.value = '';
+    } else {
+        passwordAlert.textContent = '';
     }
 });
 
@@ -34,10 +36,10 @@ function validateForm(input) {
     return input.checkValidity();
 }
 
-submit.addEventListener("click", () => {
+submit.addEventListener('click', () => {
     let inputArray = Array.from(inputElements);
     if (inputArray.every(validateForm)) {
-        console.log("Your form is valid!");
+        console.log('Your form is valid!');
     } else {
         // Find the input that is invalid
     }
